2b2580818aefa95a68501af63c1d1fd11fe354d6
tasnim-jawad <muhammadtasnimulhasan1@gmail.com>
2024-05-16 12:51:06 +0600

    user report complete

--- resources/views/admin/report/user_report.blade.php
+++ resources/views/admin/report/user_report.blade.php
@@ -76,7 +76,7 @@
                 </tbody>
             </table>
         </div>
-        {{-- <div class="payment_history mt-5 py-3 px-4">
+        <div class="payment_history mt-5 py-3 px-4">
             <h4 class="mb-0">All time Payment</h4>
             <table class="table ">
                 <thead>
@@ -88,43 +88,15 @@
                     </tr>
                 </thead>
                 <tbody id="payment_all_body">
-
-                    <tr>
-                        <td></td>
-                        <td colspan="2" class="text-end">total</td>
-                        <td></td>
-                    </tr>
                 </tbody>
             </table>
-        </div> --}}
+        </div>
     </div>
 @endsection
 @push('cjs')
     <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
     <script>
-        function batchSelect() {
-            console.log('department selected');
-            let department_id = document.getElementById('department').value;
-            axios.get(`http://127.0.0.1:8000/batch/department-wise/${department_id}`)
-                .then(response => {
-                    if(response.data.status == "success"){
-                        let batchSelect = document.getElementById('batch_id');
-                        batchSelect.innerHTML = "<option selected disabled>---- Select Batch -----</option>";
-                        response.data.data.forEach(batch => {
-                            let option = document.createElement('option');
-                            option.value = batch.id;
-                            option.text = batch.batch_name;
-                            batchSelect.appendChild(option);
-                        })
-                    }
-                })
-                .catch(error => {
-                    console.error('Error:', error);
-                });
-        }
-
         function userReport(){
-            console.log('user selected');
             let user_id = document.getElementById('user_id').value;
 
             let user_name = document.getElementById('user_name');
@@ -140,35 +112,54 @@
             let payment_monthly_body = document.getElementById('payment_monthly_body');
             let total_payment_monthly = document.getElementById('total_payment_monthly');
 
-            console.log(user_id);
             axios.get(`http://127.0.0.1:8000/report/user-report/user-search/${user_id}`)
                 .then(response => {
                     if(response.data.status == 'success'){
-                        console.log(response.data);
                         user_name.textContent = response.data.user.name;
                         user_role.textContent = response.data.user.user_role.user_role;
                         user_department.textContent = response.data.user.department;
-                        // user_name.textContent = response.data.user.batch;
+
                         total_payment.textContent = response.data.total_payment_monthly;
                         total_meal.textContent = response.data.total_meal;
                         meal_rate.textContent = Math.round(response.data.meal_rate);
                         need_to_pay.textContent = Math.round(response.data.total_meal * response.data.meal_rate);
+
                         total_payment_monthly.textContent = response.data.total_payment_monthly;
+
+                        const rowsToRemove = payment_monthly_body.querySelectorAll('tr:not(:last-child)');
+                        rowsToRemove.forEach(row => {
+                            row.remove();
+                        });
+
                         let payment_month = response.data.payment_monthly;
-                        console.log(payment_month);
-                        let index = 0;
+                        let index = payment_month.length;
                         payment_month.forEach(data => {
-                            console.log(data.month);
                             let month_name = new Date(data.month).toLocaleString('default', { month: 'long' });
-                            console.log(month_name);
                             payment_monthly_body.insertAdjacentHTML('afterbegin', `
                                                                                     <tr>
-                                                                                        <td>${index + 1}</td>
+                                                                                        <td>${index}</td>
                                                                                         <td>${month_name}</td>
                                                                                         <td>${data.payment_date}</td>
                                                                                         <td>${data.amount}</td>
                                                                                     </tr>
                                                                                 `);
+                            index--
+                        })
+
+                        payment_all_body.innerHTML = '';
+                        let payment_all =  response.data.payment_all;
+                        let i = payment_all.length;
+                        payment_all.forEach(data => {
+                            let month_name = new Date(data.month).toLocaleString('default', { month: 'long' });
+                            payment_all_body.insertAdjacentHTML('afterbegin', `
+                                                                                    <tr>
+                                                                                        <td>${i}</td>
+                                                                                        <td>${month_name}</td>
+                                                                                        <td>${data.payment_date}</td>
+                                                                                        <td>${data.amount}</td>
+                                                                                    </tr>
+                                                                                `);
+                            i--
                         })
                     }
                 })
